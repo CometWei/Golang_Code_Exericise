2476833194ac99b207db3462cf01f5e5aaa76368
AustinComet <reveno415066@gmail.com>
2021-07-01 01:48:34 +0800

    新檔案：   ToUpper/Toupper.go 	新檔案：   random_string/random_string.go

---
+++ random_string/random_string.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"math/rand"
+	"time"
+)
+
+func main() {
+	println(GetRandomString(1))
+}
+
+func GetRandomString(l int) string {
+	str := "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+	bytes := []byte(str)
+	result := []byte{}
+	r := rand.New(rand.NewSource(time.Now().UnixNano()))
+	for i := 0; i < l; i++ {
+		result = append(result, bytes[r.Intn(len(bytes))])
+	}
+	return string(result)
+}
